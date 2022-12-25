374c5a3fee34d8e36be88b6c228da7f9a0df5936
mikolajmarcinczak <mikolajtechhnika@gmail.com>
2022-12-25 22:07:49 +0100

    model

---
+++ Bookshelf_API/Models/Book.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bookshelf_API.Models
+{
+    public class Book
+    {
+        private int _id;
+        private string _author;
+        private string _title;
+        private string _description;
+        private string[] _tags;
+        private string _imagePath;
+        private DateTime _releaseDate;
+
+        [Required]
+        public int Id 
+        { 
+            get { return _id; } 
+        }
+        [Required]
+        public string Author
+        {
+            get { return _author; }
+            set { _author = value; }
+        }
+        [Required]
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        public string[] Tags
+        {
+            get { return _tags; }
+            set { _tags = value; }
+        }
+        [DataType(DataType.ImageUrl)]
+        public string ImagePath
+        {
+            get { return _imagePath; }
+            set { _imagePath = value; }
+        }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate
+        {
+            get { return _releaseDate; }
+            set { _releaseDate = value; }
+        }
+    }
+}
